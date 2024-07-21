7acab04592c0a3514b6208ebb9a74316f2ef61b2
Uğur Güneş <ugugkusu@gmail.com>
2024-07-21 22:15:30 +0300

    dj

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def hello_world():
+    if request.method == "GET":
+       return render_template("login.html")
+    
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if not username or not password:
+            error = "Kullanıcı adı yada şifre boş olamaz.."
+            return render_template("login.html", error=error)
+        if username == "mendebur" and password == "taklaci_guvercin_34":
+            return send_from_directory("uploads/", "world2.zip")
+        else:
+            error = "Kullanıcı adı yada şifre yanlış.."
+            return render_template("login.html", error=error)
+            
